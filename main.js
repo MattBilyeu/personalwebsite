b709535d466fda3cec3dc8bb2bc2910a7b56b812
Matt Bilyeu <matt.bilyeu1@gmail.com>
2023-05-29 09:18:54 -0400

    Finished Video Elements
    Finished the video elements and associated javascript.

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 function init() {
     const videoButtons = document.querySelectorAll('.elevatorPitch');
     for (let i = 0; i < videoButtons.length; i++) {
-        videoButtons[i].addEventListener('click', handleVideoButton(e.id))
+        videoButtons[i].addEventListener('click', () => {
+            handleVideoButton(videoButtons[i].id)
+        })
     }
+    const videoBackdrop = document.querySelector('.videoBackdrop');
+    videoBackdrop.addEventListener('click', ()=> {
+        handleHideVideo();
+    })
 }
 
-function handleVideoButton() {
-
+function handleVideoButton(id) {
+    const videoBackdrop = document.querySelector('.videoBackdrop');
+    videoBackdrop.style.display = 'block';
+    const video = document.querySelector('#pitchVideo');
+    video.src = id;
 }
 
 function handleHideVideo() {
     const video = document.querySelector('#pitchVideo');
     video.src = "";
-    const videoBackdrop = document.querySelector('')
+    const videoBackdrop = document.querySelector('.videoBackdrop');
+    videoBackdrop.style.display = 'none';
 }
 
 window.onload = init;
